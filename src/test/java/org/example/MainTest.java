01e37f7c498c5bb56d67d7ea2b32958275d11b55
SkaNic <106460617+KonstantinSuvorov@users.noreply.github.com>
2023-12-15 14:53:03 +0300

    Update MainTest.java
    Модификация кода теста

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,51 @@
+package org.example;
+
+/**
+ * Тест кейс для тестирования паттерна Компоновщик
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-class MainTest {
-  
+
+abstract class GraphicTest {
+    protected Graphic graphic;
+
+    @BeforeEach
+    void setUp() {
+        graphic = createGraphic();
+    }
+
+    protected abstract Graphic createGraphic();
+
+    @Test
+    void testPrint() {
+        // Проверка вывода на консоль
+        // ...
+    }
+}
+
+class EllipseTest extends GraphicTest {
+    @Override
+    protected Graphic createGraphic() {
+        return new Ellipse();
+    }
+}
+
+class CompositeGraphicTest extends GraphicTest {
+    @Override
+    protected Graphic createGraphic() {
+        CompositeGraphic compositeGraphic = new CompositeGraphic();
+        compositeGraphic.add(new Ellipse());
+        return compositeGraphic;
+    }
+
+    @Test
+    void testAdd() {
+        // Проверка добавления элементов
+    }
 }
