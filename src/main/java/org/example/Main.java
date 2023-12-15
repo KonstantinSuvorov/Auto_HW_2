e02d42a9482432cce63cfbe94d7a2c9a0e0799d6
SkaNic <106460617+KonstantinSuvorov@users.noreply.github.com>
2023-12-15 14:51:22 +0300

    Update Main.java
    Исправление кода main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,56 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Общий интерфейс для элементов и контейнеров
+interface Graphic {
+    void print();
+}
+
+// Класс компонент для элементов
+class Ellipse implements Graphic {
+    @Override
+    public void print() {
+        System.out.println("Ellipse");
+    }
+}
+
+// Класс компонент для контейнеров
+class CompositeGraphic implements Graphic {
+    private final List<Graphic> graphics = new ArrayList<>();
+
+    @Override
+    public void print() {
+        for (Graphic graphic : graphics) {
+            graphic.print();
+        }
+    }
+
+    // Элементы управления для контейнеров
+    public void add(Graphic graphic) {
+        graphics.add(graphic);
+    }
+
+    public void remove(Graphic graphic) {
+        graphics.remove(graphic);
+    }
+}
+
+// Пример использования
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ellipse ellipse1 = new Ellipse();
+        Ellipse ellipse2 = new Ellipse();
+        Ellipse ellipse3 = new Ellipse();
+
+        CompositeGraphic graphic = new CompositeGraphic();
+        CompositeGraphic graphic1 = new CompositeGraphic();
+        graphic1.add(ellipse1);
+        graphic1.add(ellipse2);
+        graphic.add(graphic1);
+        graphic.add(ellipse3);
+
+        graphic.print();
     }
 }
